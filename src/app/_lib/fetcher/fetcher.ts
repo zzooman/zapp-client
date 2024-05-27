1193ebef19b474a58a8b5aab21cabf1ed28d801a
zzoo <zzooxox@gmail.com>
2024-05-27 22:27:16 +0900

    Refactor code to upload media files to AWS S3

--- src/app/_lib/fetcher/fetcher.ts
+++ src/app/_lib/fetcher/fetcher.ts
@@ -51,5 +51,6 @@ const API = {
   signup,
   login,
   post,
+  getPosts,
 };
 export default API;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,16 @@ import Footer from '@/app/_components/common/footer/Footer';
 import Header from '@/app/_components/common/header/Header';
 
 import PostList from '@/app/_components/posts/PostList';
+import API from './_lib/fetcher/fetcher';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await API.getPosts({ limit: 10, offset: 1 });
+
   return (
     <Container>
       <Header menu />
       <main className="flex justify-center min-h-screen h-max w-full m-auto overflow-y-auto">
-        <PostList initialPosts={[]} />
+        <PostList initialPosts={posts.data} />
       </main>
       <ButtonCreateFeed show />
       <Footer />
