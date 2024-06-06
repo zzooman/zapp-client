391f67a2cb1e22726e13736e45f9ec575f949cd8
zzoo <zzooxox@gmail.com>
2024-06-07 00:31:23 +0900

    refactor: Add PostRow component to BuyListPage and SoldListPage
    - Import the PostRow component in the BuyListPage and SoldListPage files
    - Replace the existing code that renders a div with the PostRow component
    - Update the CSS classes to style the list of posts
    
    This refactor adds the PostRow component to the BuyListPage and SoldListPage files, improving the structure and readability of the code. The PostRow component is imported and used to render each post in a list format. Additionally, the CSS classes are updated to style the list of posts. These changes enhance the consistency and visual appeal of the pages.

--- src/app/mypage/sold-list/page.tsx
+++ src/app/mypage/sold-list/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import PostRow from '@/app/_components/common/post/postRow';
 import API from '@/app/_lib/fetcher/fetcher';
 import { PostWithAuthor } from '@/app/_lib/types/types';
 import { useRouter } from 'next/navigation';
@@ -26,9 +27,11 @@ export default function SoldListPage() {
   return (
     <main>
       {!loading && posts?.length === 0 && <p className="mt-32 w-full text-center">판매한 상품이 없습니다.</p>}
-      {posts?.map((post, i) => (
-        <div key={i}>post</div>
-      ))}
+      <ul className="flex flex-col divide-y divide-slate-700 my-10">
+        {posts?.map((post, i) => (
+          <PostRow key={i} post={post} />
+        ))}
+      </ul>
     </main>
   );
 }
