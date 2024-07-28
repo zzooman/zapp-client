cf7ff315f06072b21d9da2868b3aa3705a89c3ec
주영석 <ysjoo@mustit.co.kr>
2024-07-29 00:50:44 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
   images: {
     domains: ['zapp-medias.s3.ap-northeast-2.amazonaws.com'],
   },
